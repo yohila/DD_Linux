a28081c54763f21318f227c17a730be0327bb563
yohila <awais_hec@yahoo.com>
2022-04-14 16:08:01 +0200

    Adding C++

--- setup.py
+++ setup.py
@@ -4,7 +4,15 @@ from setuptools import Extension, setup, find_packages
 import os, sys
 import platform
 
-
+class custom_build_ext(build_ext):
+    def build_extensions(self):
+        # Override the compiler executables. Importantly, this
+        # removes the "default" compiler flags that would
+        # otherwise get passed on to to the compiler, i.e.,
+        # distutils.sysconfig.get_var("CFLAGS").
+        self.compiler.set_executable("compiler_so", "g++ -fpic")
+        self.compiler.set_executable("compiler_cxx", "g++")
+        build_ext.build_extensions(self)
 
 setup(
     	name="depth",
@@ -14,11 +22,20 @@ setup(
    	description="The package provides many procedures for calculating the depth of points in an empirical distribution for many notions of data depth",
    	long_description="The package provides many procedures for calculating the depth of points in an empirical distribution for many notions of data depth",
    	long_description_content_type="text/markdown",
-	platforms=['linux'],
    	url="https://github.com/pypa",
+   	platforms='linux',
     	packages=find_packages(),
+    	ext_modules=[
+        Extension(
+            "cibuild", 
+            sources=["cibuild/depth.cpp"],
+            extra_compile_args=["-I."],
+            extra_link_args=["-rdynamic",'-std=c++11','-fPIC']
+        )
+    ],
     	include_package_data=True,
   	package_data={"depth": ['ddalpha.so','depth_wrapper.so']},
+	cmdclass={"build_ext": custom_build_ext},
    	zip_safe=False,
 	)
 
